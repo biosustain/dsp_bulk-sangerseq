fc701f84cba83b323a80e9ade9bf72a47fb8cece
Sebastian Schulz <sebschu@biosustain.dtu.dk>
2025-08-06 19:04:54 +0200

    fix data path error and modify command (works with tracy --help test case but not with the tracy decompose command)

--- tracy_bulk_docker.py
+++ tracy_bulk_docker.py
@@ -8,14 +8,16 @@ import glob
 
 # %% USER SETTINGS
 
-# Folder path where data is saved (.ab1 files)
-folder_path = ['/Users/sebschu/Documents/senDS_cfb/code/tracy/data/sanger_seq']
+# Folder path where data is saved (.ab1 files) and outfiles will be saved
+data_path = '/Users/sebschu/Documents/senDS_cfb/code/dsp_bulk-sangerseq/data/sanger_seq'
+
+outdir_path = '/Users/sebschu/Documents/senDS_cfb/code/dsp_bulk-sangerseq/outdir/sanger_seq'
 
 # reference to align to
-ref_name = 'fake_template.fa'
+ref_name = 'reference.fa'
 
-# outfile path
-outfile_path = 'results/variant_trim'
+# outfile path -- Double-check with docker command
+outfile_path = 'results/bulk_test'
 
 # trimming options
 trim_left = 50
@@ -38,25 +40,29 @@ docker_image = f'geargenomics/tracy:{image_version}'
 
 #%%
 # Create list of file paths and loop over each path name and run a container for each
-file_paths = [file_path for file_path in glob.glob(os.path.join(folder_path, '*.ab1'))]
+file_paths = [file_path for file_path in glob.glob(os.path.join(data_path, '*.ab1'))]
+print(file_paths)
 
+#%%
 for file_path in file_paths:
     container_name = f'container_{file_path.split('/')[-1]}'
     docker_cmd = [
         'docker', 'run',
-        '--rm',                                  # Remove the container
-        '-v', f'{folder_path}/data:/home/sanger_seq/data:ro', # Mount data volume
-        '-v', f'{folder_path}/results:/home/sanger_seq/results' # Mount results volume
-        '--name', container_name,                # Optional: name the container
+        #'--rm',                                  # Remove the container
+        '-v', f'{data_path}:/home/sanger_seq/data', # Mount data volume
+        '-v', f'{outdir_path}:/home/sanger_seq/outdir', # Mount outdir volume
+        '--name', container_name, 
+        '-i', #-i lets the conainer actively run
         '--platform', 'linux/amd64',             # platform (precede image!)
         docker_image,
-        f'tracy decompose -v -r {ref_name} -o {outfile_path} --trimLeft {trim_left} --trimRight {trim_right} ', # command to run inside the docker container
+        'tracy', '--help'
+        #f'tracy decompose -v -r reference.fa -o test_6Aug --trimLeft {trim_left} --trimRight {trim_right} ' # command to run inside the docker container
     ]
 
     print('Running:', ' '.join(docker_cmd))
     
     try:
-        subprocess.run(' '.join(docker_cmd), check=True)
+        subprocess.run(docker_cmd, check=True)
     
     except subprocess.CalledProcessError as e:
         print(f'Error running container for {file_path}: {e}')
