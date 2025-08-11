29f47e40e8ed3ceb70361726473e8f5e2e5bb19d
Sebastian Schulz <sebschu@biosustain.dtu.dk>
2025-08-11 23:09:47 +0200

    extent script goal documentation

--- tracy_postprocesssing.py
+++ tracy_postprocesssing.py
@@ -1,5 +1,6 @@
 # Script to perform post-processing of results from script 'tracy_bulk_docker.py'
-# relevant results from json output files is extracted and consolidated into a concise results reporting table
+# relevant results from json output files are extracted and consolidated into a concise results reporting table
+# Electropherogram traces are plotted
 
 # %%
 import json
