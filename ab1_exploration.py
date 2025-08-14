a4b2bbe1140dd3a33fc4290e8cf88d26f9120129
Sebastian Schulz <sebschu@biosustain.dtu.dk>
2025-08-14 13:17:51 +0200

    move test for sanger trace plotting using biopython to the ab1_exploration  script (will not be used for now in postprocessing)

--- ab1_exploration.py
+++ ab1_exploration.py
@@ -1,1 +1,28 @@
 # script to explore the content of an ab1 file
+
+from Bio import SeqIO
+from collections import defaultdict
+import matplotlib.pyplot as plt
+
+# %% Plot electropherogram traces
+# see Biopython documentation: https://biopython.org/wiki/ABI_traces
+# and the reference therein: http://www.appliedbiosystem.com/support/software_community/ABIF_File_Format.pdf 
+# possible alternative: https://github.com/ponnhide/sangerseq_viewer
+# geargenomics teal is another viewer: https://github.com/gear-genomics/teal
+record = SeqIO.read(f'{cfg['paths']['data_host']}/EF73244592_EF73244592.ab1', 'abi')
+
+print(list(record.annotations.keys()))
+print(list(record.annotations['abif_raw'].keys()))
+
+
+channels = ['DATA9', 'DATA10', 'DATA11', 'DATA12']
+trace = defaultdict(list)
+for c in channels:
+    trace[c] = record.annotations["abif_raw"][c]
+
+# plot data 
+plt.plot(trace['DATA9'], color='blue')
+plt.plot(trace['DATA10'], color='red')
+plt.plot(trace['DATA11'], color='green')
+plt.plot(trace['DATA12'], color='yellow')
+plt.show()
--- tracy_postprocesssing.py
+++ tracy_postprocesssing.py
@@ -52,26 +52,4 @@ for sample_name, result_df in result_df_dict.items():
     result_df.to_csv(f'{cfg['paths']['outdir_host']}/{sample_name}.csv')
 
 
-# %% Plot electropherogram traces
-# see Biopython documentation: https://biopython.org/wiki/ABI_traces
-# and the reference therein: http://www.appliedbiosystem.com/support/software_community/ABIF_File_Format.pdf 
-# possible alternative: https://github.com/ponnhide/sangerseq_viewer
-# geargenomics teal is another viewer: https://github.com/gear-genomics/teal
-record = SeqIO.read(f'{cfg['paths']['data_host']}/EF73244592_EF73244592.ab1', 'abi')
-
-print(list(record.annotations.keys()))
-print(list(record.annotations['abif_raw'].keys()))
-
-
-channels = ['DATA9', 'DATA10', 'DATA11', 'DATA12']
-trace = defaultdict(list)
-for c in channels:
-    trace[c] = record.annotations["abif_raw"][c]
-
-# plot data 
-plt.plot(trace['DATA9'], color='blue')
-plt.plot(trace['DATA10'], color='red')
-plt.plot(trace['DATA11'], color='green')
-plt.plot(trace['DATA12'], color='yellow')
-plt.show()
 # %%
