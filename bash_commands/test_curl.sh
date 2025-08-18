e17ba1a1f843e7afcc703018088521796eb0ebbd
Sebastian Schulz <sebschu@biosustain.dtu.dk>
2025-08-18 13:08:08 +0200

    curl commands for geargenomics API (sanger trace viewer)

---
+++ bash_commands/test_curl.sh
@@ -0,0 +1,6 @@
+#Upload
+curl -X POST -F "queryFile=@/Users/sebschu/Documents/senDS_cfb/code/dsp_bulk-sangerseq/bash_commands/EF73244592_EF73244592.ab1" "https://gear-genomics.embl.de/teal/api/v1/upload" -o test_curl
+
+
+
+  
