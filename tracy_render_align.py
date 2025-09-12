5bd9c2bb827e05ecd7701640d0db438c18a3b894
Dominik Mariusz Maszczyk <dommas@biosustain.dtu.dk>
2025-09-12 13:16:45 +0200

    feat: add script to render align plots

---
+++ tracy_render_align.py
@@ -0,0 +1,43 @@
+import glob
+import os
+from pathlib import Path
+import shutil
+
+import yaml
+
+# %% Read yaml configuration file
+with open("./config.yaml", "r") as file:
+    cfg = yaml.safe_load(file)
+
+align_output_dir = os.path.join(cfg["paths"]["outdir_host"], "align")
+if not os.path.exists(align_output_dir):
+    raise Exception(f"Alignment output directory does not exist: {align_output_dir}")
+
+shutil.copy(Path("static", "traceView.js"), align_output_dir)
+
+
+def viewer_html_factory(json_file):
+    return f"""<!DOCTYPE html>
+<html>
+<head>
+    <title>Trace Viewer</title>
+</head>
+<body>
+    <div id="traceView"></div>
+    <script src="./traceView.js"></script>
+    <script>
+
+        document.addEventListener("DOMContentLoaded", () => displayData({json_str}));
+    </script>
+</body>
+</html>
+    """
+
+
+for path_str in glob.glob(os.path.join(cfg["paths"]["outdir_host"], "align", "*.json")):
+    path = Path(path_str)
+    with open(path, "r") as f:
+        json_str = f.read()
+    html_content = viewer_html_factory(json_str)
+    with open(os.path.join(align_output_dir, f"{path.stem}.html"), "w") as f:
+        f.write(html_content)
