41fd87f7cb0d15650ce198e3e9984fbb6978ebb5
Dominik Mariusz Maszczyk <dommas@biosustain.dtu.dk>
2025-09-24 15:12:43 +0200

    feat: laverage docker compose to orchestrate containers

--- tracy_bulk_docker_samplesheet.py
+++ tracy_bulk_docker_samplesheet.py
@@ -4,7 +4,6 @@
 #%%
 import subprocess
 import os
-import glob
 import yaml
 import pandas as pd
 
@@ -12,14 +11,6 @@ import pandas as pd
 # %% Read yaml configuration file
 with open('./config.yaml', 'r') as file:
      cfg = yaml.safe_load(file)
-
-
-# %% Download Docker image
-try:
-    subprocess.run(['docker', 'pull', f'{cfg['docker']['image']}:{cfg['docker']['version']}'], check=True)
-    
-except subprocess.CalledProcessError as e:
-        print(f'Error pulling image {cfg['docker']['image']}:{cfg['docker']['version']}: {e}')
 
 
 # %% Perform sequencing analysis
@@ -49,77 +40,30 @@ for sample_ref_pair in sample_ref_pairs:
     reference_name = sample_ref_pair.reference.split('/')[-1]
 
     docker_cmd = [
-        'docker', 'run',
-        # Remove the container
-        '--rm',                                  
-        # Mount data volume (ro: read-only)
-        '-v', f'{cfg['paths']['data_host']}:{cfg['paths']['data_docker']}:ro',
-        # Mount outdir volume
-        '-v', f'{cfg['paths']['outdir_host']}:{cfg['paths']['outdir_docker']}', 
-        # container name
-        '--name', f'decompose_{sample_id}', 
-        # -i option lets the conainer actively run
-        '-i',                               
-        # platform (precede image!)
-        '--platform', 'linux/amd64',             
-        # docker image and version to use
-        f'{cfg['docker']['image']}:{cfg['docker']['version']}',
-        
-        # tracy decompose command for variant calling
-        'tracy', 'decompose', '-v',
-        # reference to align to
-        '-r', f'{cfg['paths']['data_docker']}/{reference_name}',
-        # outdirectory and outfile name
-        '-o', f'{cfg['paths']['outdir_docker']}/{sample_id}',
-        # sequence trimming options
-        '--trimLeft', f'{cfg['tracy']['trim_left']}',
-        '--trimRight', f'{cfg['tracy']['trim_right']}', 
-        # .ab1 file to use
-        f'{cfg['paths']['data_docker']}/{file_name}'
+        "docker",
+        "compose",
+        "-p",
+        str(sample_id),
+        "up"
     ]
 
-    print('Running decompose:', ' '.join(docker_cmd))
-    
-    try:
-        subprocess.run(docker_cmd, check=True)
-    
-    except subprocess.CalledProcessError as e:
-        print(f'Error running container for {sample_ref_pair.ab1_1}: {e}')
+    params = dict(
+        REFERENCE_NAME=reference_name,
+        SAMPLE_ID=sample_id,
+        FILE_NAME=file_name,
+        TRIM_LEFT=str(cfg["tracy"]["trim_left"]),
+        TRIM_RIGHT=str(cfg["tracy"]["trim_right"]),
+        DATA_HOST=os.path.abspath(cfg["paths"]["data_host"]),
+        OUTDIR_HOST=os.path.abspath(cfg["paths"]["outdir_host"]),
+    )
 
-    docker_cmd = [
-        'docker', 'run',
-        # Remove the container
-        '--rm',                                  
-        # Mount data volume (ro: read-only)
-        '-v', f'{cfg['paths']['data_host']}:{cfg['paths']['data_docker']}:ro',
-        # Mount outdir volume
-        '-v', f'{cfg['paths']['outdir_host']}/align:{cfg['paths']['outdir_docker']}', 
-        # container name
-        '--name', f'align_{sample_id}', 
-        # -i option lets the conainer actively run
-        '-i',                               
-        # platform (precede image!)
-        '--platform', 'linux/amd64',             
-        # docker image and version to use
-        f'{cfg['docker']['image']}:{cfg['docker']['version']}',
-        
-        # tracy align command for variant calling
-        'tracy', 'align',
-        # reference to align to
-        '-r', f'{cfg['paths']['data_docker']}/{reference_name}',
-        # outdirectory and outfile name
-        '-o', f'{cfg['paths']['outdir_docker']}/{sample_id}',
-        # sequence trimming options
-        '--trimLeft', f'{cfg['tracy']['trim_left']}',
-        '--trimRight', f'{cfg['tracy']['trim_right']}', 
-        # .ab1 file to use
-        f'{cfg['paths']['data_docker']}/{file_name}'
-    ]
+    print("Running:", " ".join(docker_cmd))
+    print("With env params:\n", '\n'.join([f"\t{k}={v}" for k, v in params.items()]))
 
-    print('Running align:', ' '.join(docker_cmd))
-    
     try:
-        subprocess.run(docker_cmd, check=True)
-    
+        subprocess.run(docker_cmd, check=True, env=dict(os.environ, **params))
     except subprocess.CalledProcessError as e:
-        print(f'Error running container for {sample_ref_pair.ab1_1}: {e}')
+        print(f"Error for {sample_ref_pair.ab1_1}: {e}")
+    finally:
+        # tear down the containers after run
+        subprocess.run(['docker', 'compose', '-p', str(sample_id), 'down'])
