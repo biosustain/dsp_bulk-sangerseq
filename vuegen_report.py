94afa06256ce0bd3b157c4dabf0c03c56e72a585
Sebastian Schulz <sebschu@biosustain.dtu.dk>
2026-04-09 18:05:46 +0200

    copy and display alignments and consensus sequences from tracy assemble process to display them in vuegen

--- vuegen_report.py
+++ vuegen_report.py
@@ -8,7 +8,7 @@ import yaml
 from util.helper_functions import copy_files, copy_files_extension,  convert_files_to_markdown_read_write
 
 
-# %% read relevant onfig parameters
+# %% read relevant config parameters
 with open(Path('./config.yaml'), 'r', encoding='utf-8') as file:
     cfg = yaml.safe_load(file)
 
@@ -18,12 +18,14 @@ outdir_host = Path(cfg['paths']['outdir_host'])
 # %% define variables
 outdir_vuegen = Path(os.path.join(outdir_host, 'vuegen_report'))
 
-#sub-directory names where files will be copied to
+#sub-directory and sub-sub-directory names where files will be copied to
 subdir_names = ['01_Mutation_tables_decompose', 
                 '02_alignments_decompose', 
-                '03_alignments_align'
+                '03_alignments_align', 
+                '04_sequence_assembly_assemble'
                 ]
 subdir_names_alignments_decompose_02 = ['align1', 'align2', 'align3']
+subdir_names_seq_assemble_04 = ['alignments', 'consensus_sequences']
 
 #mutation table output to be copied
 src_dir_mut_comb = Path('outdir/results_combined.csv')
@@ -38,27 +40,33 @@ src_dir_align = Path('./outdir/align')    #source dir
 dest_dir_align = Path(f'./outdir/vuegen_report/{subdir_names[2]}')    #destination dir
 file_ext_align = ('.txt') #file extensions
 
-#files to copy from assembly process
+#files to copy from assembly dir
+src_dir_assemble = Path('outdir/assemble')
+dest_dir_assemble = Path(f'./outdir/vuegen_report/{subdir_names[3]}')
 
-#TO BE ADDED 
 
+# %% create vuegen directory and relevant sub-folders
 
-# %% create vuegen folder and relevant sub-folders
+#vuegen directory
 for subdir_name in subdir_names:
     os.makedirs(Path(os.path.join(outdir_vuegen, subdir_name)), exist_ok=True)
 
-
+#sub-directories of the 02_alignments_decompose directory
 for subdir in subdir_names_alignments_decompose_02:
     os.makedirs(Path(os.path.join(dest_dir_decompose, subdir)), exist_ok=True)
+
+#sub-directories of the 04_sequence_assembly_assemble directory
+for subdir_assemble in subdir_names_seq_assemble_04:
+    os.makedirs(Path(os.path.join(dest_dir_assemble, subdir_assemble)), exist_ok=True)
 
 
 # %% copy results files into vuegen folder and convert .align* files to markdown (.md)
 
-#copy mutation summary file (from decompose process)
+##copy mutation summary file (from decompose process)
 copy_files(src_file=src_dir_mut_comb, 
            dest_file=dest_dir_mut_comb)
 
-#copy files from decompose dir as markdowns
+##copy files from decompose dir as markdowns
 for i in subdir_names_alignments_decompose_02:
     convert_files_to_markdown_read_write(
                 src_dir_path=src_dir_decompose, 
@@ -66,11 +74,27 @@ for i in subdir_names_alignments_decompose_02:
                 dest_dir_path=f'{dest_dir_decompose}/{i}'
                 )
 
-#copy files from align dir as markdowns
+##copy files from align dir as markdowns
 convert_files_to_markdown_read_write(
                 src_dir_path=src_dir_align, 
                 src_file_ext=file_ext_align, 
                 dest_dir_path=dest_dir_align
+                )
+
+##copy files from assemble dir as markdowns
+
+#alignments
+convert_files_to_markdown_read_write(
+        src_dir_path=src_dir_assemble, 
+        src_file_ext='.align.fa', 
+        dest_dir_path=f'{dest_dir_assemble}/{subdir_names_seq_assemble_04[0]}'
+                )
+
+#consensus sequences
+convert_files_to_markdown_read_write(
+        src_dir_path=src_dir_assemble, 
+        src_file_ext='.cons.fa', 
+        dest_dir_path=f'{dest_dir_assemble}/{subdir_names_seq_assemble_04[1]}'
                 )
 
 # %% create vuegen report
