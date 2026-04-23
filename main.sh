8e55ee23c3b9fe2d9820dff1c0ab4b0d20eec219
Sebastian Schulz <sebschu@biosustain.dtu.dk>
2026-04-23 22:59:06 +0200

    correct package.module call

--- main.sh
+++ main.sh
@@ -7,7 +7,7 @@ python3 -m bin.tracy_bulk_docker_samplesheet.py
 python3 -m bin.tracy_postprocesssing.py
 
 # Render visualisations
-python3 -m tracy_render_visualisations.py
+python3 -m bin.tracy_render_visualisations.py
 
 # Create vuegen report
-python3 -m vuegen_report.py
+python3 -m bin.vuegen_report.py
