71a6c70e8bc955d3ffe3263935be114c92a8bc03
Sebastian Schulz <sebschu@biosustain.dtu.dk>
2026-05-06 15:13:55 +0200

    correct module calls

--- main.sh
+++ main.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 # Run Sanger sequencing analysis script
-python3 -m bin.tracy_bulk_docker_samplesheet.py
+python3 -m bin.tracy_bulk_docker_samplesheet
 
 # Run postprocessing script
-python3 -m bin.tracy_postprocesssing.py
+python3 -m bin.tracy_postprocesssing
 
 # Render visualisations
-python3 -m bin.tracy_render_visualisations.py
+python3 -m bin.tracy_render_visualisations
 
 # Create vuegen report
-python3 -m bin.vuegen_report.py
+python3 -m bin.vuegen_report
